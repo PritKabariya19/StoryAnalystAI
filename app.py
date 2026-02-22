3885f53e61462cc02e1a985f8e7d8634a5b67a50
PritKabariya19 <kabariyaprit@gmail.com>
2026-02-22 11:10:36 +0530

    Use environment variable for server port
    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -169,6 +169,9 @@ def serve_screenshot(filename):
     return send_from_directory(screenshots_dir, filename)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
